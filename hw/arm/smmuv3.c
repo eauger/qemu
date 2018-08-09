bd75ae4debbd1e1d99e5346460a741223af49f2b
Eric Auger <eric.auger@redhat.com>
2018-08-09 21:04:19 +0200

    hw/arm/smmuv3: Notify on config changes
    In case IOMMU config notifiers are attached to the
    IOMMU memory region, we execute them, passing as argument
    the iommu_pasid_table_config struct updated with the new
    viommu translation config. Config notifiers are called on
    STE changes. At physical level, they translate into
    CMD_CFGI_STE_* commands.
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    
    ---
    v2 -> v3:
    - adapt to pasid_cfg field changes
    
    v1 -> v2:
    - do not notify anymore on CD change. Anyway the smmuv3 linux
      driver is not sending any CD invalidation commands. If we were
      to propagate CD invalidation commands, we would use the
      CACHE_INVALIDATE VFIO ioctl.
    - notify a precise config flags to prepare for addition of new
      flags

--- hw/arm/smmuv3.c
+++ hw/arm/smmuv3.c
@@ -16,6 +16,8 @@
  * with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "linux/iommu.h"
+
 #include "qemu/osdep.h"
 #include "hw/boards.h"
 #include "sysemu/sysemu.h"
@@ -843,6 +845,50 @@ static void smmuv3_inv_notifiers_iova(SMMUState *s, int asid, dma_addr_t iova)
     }
 }
 
+static void smmuv3_notify_config_change(SMMUState *bs, uint32_t sid)
+{
+    IOMMUMemoryRegion *mr = smmu_iommu_mr(bs, sid);
+    SMMUEventInfo event = {.type = SMMU_EVT_NONE, .sid = sid};
+    SMMUTransCfg *cfg;
+    SMMUDevice *sdev;
+
+    if (!mr) {
+        return;
+    }
+
+    sdev = container_of(mr, SMMUDevice, iommu);
+
+    /* flush QEMU config cache */
+    smmuv3_flush_config(sdev);
+
+    if (mr->iommu_notify_flags & IOMMU_NOTIFIER_PASID_CFG) {
+        /* force a guest RAM config structure decoding */
+        cfg = smmuv3_get_config(sdev, &event);
+
+        if (cfg) {
+            IOMMUConfig *iommu_config = g_new0(IOMMUConfig, 1);
+
+            iommu_config->pasid_cfg.format = IOMMU_PASID_FORMAT_SMMUV3;
+            if (cfg->disabled || cfg->bypassed) {
+                iommu_config->pasid_cfg.config = IOMMU_PASID_CONFIG_BYPASS;
+            } else if (cfg->aborted) {
+                iommu_config->pasid_cfg.config = IOMMU_PASID_CONFIG_ABORT;
+            } else {
+                iommu_config->pasid_cfg.config = IOMMU_PASID_CONFIG_TRANSLATE;
+            }
+            iommu_config->pasid_cfg.base_ptr = cfg->s1ctxptr;
+
+            memory_region_config_notify_iommu(mr, 0, IOMMU_NOTIFIER_PASID_CFG,
+                                              iommu_config);
+            g_free(iommu_config);
+        } else {
+            qemu_log_mask(LOG_GUEST_ERROR,
+                          "%s error decoding the configuration for iommu mr=%s\n",
+                         __func__, mr->parent_obj.name);
+        }
+    }
+}
+
 static int smmuv3_cmdq_consume(SMMUv3State *s)
 {
     SMMUState *bs = ARM_SMMU(s);
@@ -893,22 +939,14 @@ static int smmuv3_cmdq_consume(SMMUv3State *s)
         case SMMU_CMD_CFGI_STE:
         {
             uint32_t sid = CMD_SID(&cmd);
-            IOMMUMemoryRegion *mr = smmu_iommu_mr(bs, sid);
-            SMMUDevice *sdev;
 
             if (CMD_SSEC(&cmd)) {
                 cmd_error = SMMU_CERROR_ILL;
                 break;
             }
 
-            if (!mr) {
-                break;
-            }
-
             trace_smmuv3_cmdq_cfgi_ste(sid);
-            sdev = container_of(mr, SMMUDevice, iommu);
-            smmuv3_flush_config(sdev);
-
+            smmuv3_notify_config_change(bs, sid);
             break;
         }
         case SMMU_CMD_CFGI_STE_RANGE: /* same as SMMU_CMD_CFGI_ALL */
@@ -925,14 +963,7 @@ static int smmuv3_cmdq_consume(SMMUv3State *s)
             trace_smmuv3_cmdq_cfgi_ste_range(start, end);
 
             for (i = start; i <= end; i++) {
-                IOMMUMemoryRegion *mr = smmu_iommu_mr(bs, i);
-                SMMUDevice *sdev;
-
-                if (!mr) {
-                    continue;
-                }
-                sdev = container_of(mr, SMMUDevice, iommu);
-                smmuv3_flush_config(sdev);
+                smmuv3_notify_config_change(bs, i);
             }
             break;
         }
