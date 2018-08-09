f1852f25472e701c387d597b23ed82bc85258212
Eric Auger <eric.auger@redhat.com>
2018-08-09 21:04:19 +0200

    hw/arm/smmuv3: Pass stage 1 configurations to the host
    In case PASID PciOps are set for the device we call
    the set_pasid_table() callback on each STE update.
    
    This allows to pass the guest stage 1 configuration
    to the host and apply it at physical level.
    
    Change-Id: I1f20643f0bdd190217cfffbb2152eeb78e2b0a50
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    
    ---
    
    [nicolinc: switch to IOMMUFD using the helper that we added]
    
    v4 -> v5:
    - Use PciOps instead of config notifiers
    
    v3 -> v4:
    - fix compile issue with mingw
    
    v2 -> v3:
    - adapt to pasid_cfg field changes. Use local variable
    - add trace event
    - set version fields
    - use CONFIG_PASID
    
    v1 -> v2:
    - do not notify anymore on CD change. Anyway the smmuv3 linux
      driver is not sending any CD invalidation commands. If we were
      to propagate CD invalidation commands, we would use the
      CACHE_INVALIDATE VFIO ioctl.
    - notify a precise config flags to prepare for addition of new
      flags

--- hw/arm/smmuv3.c
+++ hw/arm/smmuv3.c
@@ -16,6 +16,10 @@
  * with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
+#ifdef __linux__
+#include "linux/iommu.h"
+#endif
+
 #include "qemu/osdep.h"
 #include "qemu/bitops.h"
 #include "hw/irq.h"
@@ -902,6 +906,62 @@ static void smmuv3_s1_range_inval(SMMUState *s, Cmd *cmd)
     }
 }
 
+static void smmuv3_notify_config_change(SMMUState *bs, uint32_t sid)
+{
+#ifdef __linux__
+    IOMMUMemoryRegion *mr = smmu_iommu_mr(bs, sid);
+    SMMUEventInfo event = {.type = SMMU_EVT_NONE, .sid = sid,
+                           .inval_ste_allowed = true};
+    union iommu_stage1_config iommu_config = {};
+    SMMUTransCfg *cfg;
+    SMMUDevice *sdev;
+    int ret;
+
+    if (!mr) {
+        return;
+    }
+
+    sdev = container_of(mr, SMMUDevice, iommu);
+
+    /* flush QEMU config cache */
+    smmuv3_flush_config(sdev);
+
+    if (bs->iommufd < 0 || !sdev->idev) {
+	    return;
+    }
+
+    cfg = smmuv3_get_config(sdev, &event);
+    if (!cfg || !cfg->s1ctxptr) {
+        return;
+    }
+
+    iommu_config.smmu.format = IOMMU_SMMU_FORMAT_SMMUV3;
+    iommu_config.smmu.vmid = 0; /* TODO pass bs->iommufd */
+    iommu_config.smmu.s1cdmax = cfg->s1cdmax;
+    iommu_config.smmu.s1fmt = cfg->s1fmt;
+    iommu_config.smmu.s1dss = cfg->s1dss;
+
+    if (cfg->disabled || cfg->bypassed) {
+        iommu_config.smmu.config = IOMMU_SMMU_CONFIG_BYPASS;
+    } else if (cfg->aborted) {
+        iommu_config.smmu.config = IOMMU_SMMU_CONFIG_ABORT;
+    } else {
+        iommu_config.smmu.config = IOMMU_SMMU_CONFIG_TRANSLATE;
+    }
+
+    trace_smmuv3_notify_config_change(mr->parent_obj.name,
+                                      iommu_config.smmu.config,
+                                      cfg->s1ctxptr);
+
+    ret = smmu_iommu_alloc_s1_hwpt(bs, sdev, cfg->s1ctxptr, &iommu_config,
+                                   NULL);
+    if (ret) {
+        error_report("Unable to alloc Stage-1 HW Page Table: %d", ret);
+        return;
+    }
+#endif
+}
+
 static gboolean
 smmuv3_invalidate_ste(gpointer key, gpointer value, gpointer user_data)
 {
@@ -912,6 +972,7 @@ smmuv3_invalidate_ste(gpointer key, gpointer value, gpointer user_data)
     if (sid < sid_range->start || sid > sid_range->end) {
         return false;
     }
+    smmuv3_notify_config_change(sid_range->state, sid);
     trace_smmuv3_config_cache_inv(sid);
     return true;
 }
@@ -966,22 +1027,14 @@ static int smmuv3_cmdq_consume(SMMUv3State *s)
         case SMMU_CMD_CFGI_STE:
         {
             uint32_t sid = CMD_SID(&cmd);
-            IOMMUMemoryRegion *mr = smmu_iommu_mr(bs, sid);
-            SMMUDevice *sdev;
 
             if (CMD_SSEC(&cmd)) {
                 cmd_error = SMMU_CERROR_ILL;
                 break;
             }
 
-            if (!mr) {
-                break;
-            }
-
             trace_smmuv3_cmdq_cfgi_ste(sid);
-            sdev = container_of(mr, SMMUDevice, iommu);
-            smmuv3_flush_config(sdev);
-
+            smmuv3_notify_config_change(bs, sid);
             break;
         }
         case SMMU_CMD_CFGI_STE_RANGE: /* same as SMMU_CMD_CFGI_ALL */
@@ -996,6 +1049,7 @@ static int smmuv3_cmdq_consume(SMMUv3State *s)
             }
 
             mask = (1ULL << (range + 1)) - 1;
+            sid_range.state = bs;
             sid_range.start = sid & ~mask;
             sid_range.end = sid_range.start + mask;
 
