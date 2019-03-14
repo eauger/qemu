293cf4698a6733926b6b20a763e8c056742fec27
Eric Auger <eric.auger@redhat.com>
2019-03-14 09:55:13 -0400

    hw/arm/smmuv3: Fill the IOTLBEntry leaf and granule fields on NH_VA invalidation
    Let's propagate the leaf attribute and granule info throughout
    the invalidation path.
    
    The leaf hint is used to reduce the scope of the invalidations to the
    last level of translation. Not enforcing it induces large performance
    penalties in nested mode.
    
    Change-Id: I5ff4ea0a4beee6b120be3eccc1563bcce732a1dc
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    
    ---
    
    [nicolinc: switch to IOMMUFD using the helper that we added]
    
    v9 -> v10:
    - set the granule flag only in RIL mode

--- hw/arm/smmuv3.c
+++ hw/arm/smmuv3.c
@@ -810,7 +810,7 @@ epilogue:
 static void smmuv3_notify_iova(IOMMUMemoryRegion *mr,
                                IOMMUNotifier *n,
                                int asid, dma_addr_t iova,
-                               uint8_t tg, uint64_t num_pages)
+                               uint8_t tg, uint64_t num_pages, bool leaf)
 {
     SMMUDevice *sdev = container_of(mr, SMMUDevice, iommu);
     IOMMUTLBEvent event = {};
@@ -876,7 +876,7 @@ static void smmuv3_notify_asid(IOMMUMemoryRegion *mr,
 
 /* invalidate an asid/iova range tuple in all mr's */
 static void smmuv3_inv_notifiers_iova(SMMUState *s, int asid, dma_addr_t iova,
-                                      uint8_t tg, uint64_t num_pages)
+                                      uint8_t tg, uint64_t num_pages, bool leaf)
 {
     SMMUDevice *sdev;
 
@@ -888,7 +888,7 @@ static void smmuv3_inv_notifiers_iova(SMMUState *s, int asid, dma_addr_t iova,
                                         tg, num_pages);
 
         IOMMU_NOTIFIER_FOREACH(n, mr) {
-            smmuv3_notify_iova(mr, n, asid, iova, tg, num_pages);
+            smmuv3_notify_iova(mr, n, asid, iova, tg, num_pages, leaf);
         }
     }
 }
@@ -913,7 +913,7 @@ static void smmuv3_s1_range_inval(SMMUState *s, Cmd *cmd)
 
     if (!tg) {
         trace_smmuv3_s1_range_inval(vmid, asid, addr, tg, 1, ttl, leaf);
-        smmuv3_inv_notifiers_iova(s, asid, addr, tg, 1);
+        smmuv3_inv_notifiers_iova(s, asid, addr, tg, 1, leaf);
         smmu_iotlb_inv_iova(s, asid, addr, tg, 1, ttl);
         return;
     }
@@ -931,7 +931,7 @@ static void smmuv3_s1_range_inval(SMMUState *s, Cmd *cmd)
 
         num_pages = (mask + 1) >> granule;
         trace_smmuv3_s1_range_inval(vmid, asid, addr, tg, num_pages, ttl, leaf);
-        smmuv3_inv_notifiers_iova(s, asid, addr, tg, num_pages);
+        smmuv3_inv_notifiers_iova(s, asid, addr, tg, num_pages, leaf);
         smmu_iotlb_inv_iova(s, asid, addr, tg, num_pages, ttl);
         addr += mask + 1;
     }
