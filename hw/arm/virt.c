a1746c1136c7ef372d808453a77ef7c0a121daa2
Eric Auger <eric.auger@redhat.com>
2021-10-04 13:25:39 -0400

    hw/arm/virt-acpi-build: Add IORT RMR regions to handle MSI nested binding
    To handle SMMUv3 nested stage support it is practical to
    expose the guest with reserved memory regions (RMRs)
    covering the IOVAs used by the host kernel to map
    physical MSI doorbells.
    
    Those IOVAs belong to [0x8000000, 0x8100000] matching
    MSI_IOVA_BASE and MSI_IOVA_LENGTH definitions in kernel
    arm-smmu-v3 driver. This is the window used to allocate
    IOVAs matching physical MSI doorbells.
    
    With those RMRs, the guest is forced to use a flat mapping
    for this range. Hence the assigned device is programmed
    with one IOVA from this range. Stage 1, owned by the guest
    has a flat mapping for this IOVA. Stage2, owned by the VMM
    then enforces a mapping from this IOVA to the physical
    MSI doorbell.
    
    At IORT table level, due to the single mapping flag being
    set on the ID mapping, 256 IORT RMR nodes need to be
    created per bus. This looks awkward from a specification
    and implementation point of view.
    
    This may also produce a warning at execution time:
    qemu-system-aarch64: warning: ACPI table size 114709 exceeds
    65536 bytes, migration may not work
    (here with 5 pcie root ports, ie. 256 * 6 = 1536 RMR nodes!).
    
    The creation of those RMR nodes only is relevant if nested
    stage SMMU is in use, along with VFIO. As VFIO devices can be
    hotplugged, all RMRs need to be created in advance. Hence
    the patch introduces a new arm virt "nested-smmuv3" iommu type.
    
    ARM DEN 0049E.b IORT specification also mandates that when
    RMRs are present, the OS must preserve PCIe configuration
    performed by the boot FW. So along with the RMR IORT nodes,
    a _DSM function #5, as defined by PCI FIRMWARE SPECIFICATION
    EVISION 3.3, chapter 4.6.5 is added to PCIe host bridge
    and PCIe expander bridge objects.
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Suggested-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    
    ---
    
    v1 -> v2:
    - add DSM #5
    - use identifier increment
    
    Instead of introducing a new IOMMU type, we could introduce
    an array of qdev_prop_reserved_region(s).
    
    Guest can parse the IORT RMR nodes with Shammer's series:
    [PATCH v7 0/9] ACPI/IORT: Support for IORT RMR node
    
    The patch applies on Igor's v4 series [1]+ IORT E.b upgrade [2]
    [1] [PATCH v4 00/35] acpi: refactor error prone build_header()
        and packed structures usage in ACPI tables
    [2] [PATCH 0/3] hw/arm/virt_acpi_build: Upgrate the IORT table
        up to revision E.b

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -1251,7 +1251,7 @@ static void create_smmu(const VirtMachineState *vms,
     DeviceState *dev;
     MachineState *ms = MACHINE(vms);
 
-    if (vms->iommu != VIRT_IOMMU_SMMUV3 || !vms->iommu_phandle) {
+    if (!virt_has_smmuv3(vms) || !vms->iommu_phandle) {
         return;
     }
 
@@ -1441,6 +1441,7 @@ static void create_pcie(VirtMachineState *vms)
 
         switch (vms->iommu) {
         case VIRT_IOMMU_SMMUV3:
+        case VIRT_IOMMU_NESTED_SMMUV3:
             create_smmu(vms, vms->bus);
             qemu_fdt_setprop_cells(ms->fdt, nodename, "iommu-map",
                                    0x0, vms->iommu_phandle, 0x0, 0x10000);
@@ -2314,6 +2315,8 @@ static char *virt_get_iommu(Object *obj, Error **errp)
         return g_strdup("none");
     case VIRT_IOMMU_SMMUV3:
         return g_strdup("smmuv3");
+    case VIRT_IOMMU_NESTED_SMMUV3:
+        return g_strdup("nested-smmuv3");
     default:
         g_assert_not_reached();
     }
@@ -2325,6 +2328,8 @@ static void virt_set_iommu(Object *obj, const char *value, Error **errp)
 
     if (!strcmp(value, "smmuv3")) {
         vms->iommu = VIRT_IOMMU_SMMUV3;
+    } else if (!strcmp(value, "nested-smmuv3")) {
+        vms->iommu = VIRT_IOMMU_NESTED_SMMUV3;
     } else if (!strcmp(value, "none")) {
         vms->iommu = VIRT_IOMMU_NONE;
     } else {
