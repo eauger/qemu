d4305da0829363bf333d925d8671e11f6092be0b
Yi Liu <yi.l.liu@intel.com>
2022-04-04 09:10:52 -0700

    vfio: Test IOAS_COPY_DMA
    Next step to add IOAS_COPY_DMA to take the advantage of iommufd to
    save page counting. This commit should be reverted finally.
    
    This would require to move the memory_listener to be per-address
    space. Each time got the region_add, region_del notification, loop
    all containers within the as. Plan to add a root container, which
    all the map will be done, for other containers, just copy from the
    root container. Needs to consider how the dirty sync callbacks in
    the memory_listener should work. Will go to do this.
    
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>

--- hw/vfio/iommufd.c
+++ hw/vfio/iommufd.c
@@ -37,9 +37,38 @@ static int iommufd_map(VFIOIOMMUObj *iommu, hwaddr iova,
                        ram_addr_t size, void *vaddr, bool readonly)
 {
     VFIOIOMMUFDContainer *container = container_of(iommu, VFIOIOMMUFDContainer, obj);
+    int ret, iommufd;
+    uint32_t ioas_id;
 
-    return iommufd_map_dma(container->iommufd, container->ioas_id,
+    if (iova >= 0xfe000000)
+        printf("%s - 1\n", __func__);
+    ret = iommu_get_ioas(&iommufd, &ioas_id);
+    if (ret < 0) {
+    if (iova >= 0xfe000000)
+        printf("%s fail to get ioas_id\n", __func__);
+	return ret;
+    } else {
+        if (iova >= 0xfe000000)
+        printf("%s got iommufd: %d ioas_id: %u\n", __func__, iommufd, ioas_id);
+    }
+
+    ret = iommufd_map_dma(iommufd, ioas_id,
                            iova, size, vaddr, readonly);
+    if (!ret) {
+    if (iova >= 0xfe000000)
+        printf("%s succ to map dummy ioas_id\n", __func__);
+    } else {
+    if (iova >= 0xfe000000)
+        printf("%s fail to map dummy ioas_id\n", __func__);
+	return ret;
+    }
+    if (iova >= 0xfe000000)
+    printf("%s copy dummy ioas_id to target ioas_id\n", __func__);
+    ret = iommufd_copy_dma(container->iommufd, ioas_id, container->ioas_id,
+		           iova, size, vaddr, readonly);
+    if (iova >= 0xfe000000)
+    printf("%s copy dummy ioas_id to target ioas_id %s\n", __func__, ret?"failed":"succ");
+    return ret;
 }
 
 static int iommufd_unmap(VFIOIOMMUObj *iommu,
